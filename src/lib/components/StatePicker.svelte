d255a88c57a6a031a2d7832f1953b83071a86c08
Alec Iberg <ibergalec@gmail.com>
2023-08-20 11:52:26 -0700

    refactor, active state state working

---
+++ src/lib/components/StatePicker.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+	import { STATES } from '$lib/constants';
+	import { supabase } from '$lib/supabaseClient';
+
+	export let activeState;
+	let selected = activeState.state_name;
+
+	const stateChanger = async () => {
+		let data = await supabase.from('states').select('*').eq('state_name', selected);
+		if (data.data == undefined) {
+			console.error('Error in updating active state');
+		} else {
+			activeState = data.data[0];
+		}
+	};
+</script>
+
+<div class="select-container">
+	<h1>Select a State</h1>
+	<span>
+		<select bind:value={selected} on:change={stateChanger}>
+			{#each STATES as state}
+				<option value={state}>
+					{state}
+				</option>
+			{/each}
+		</select>
+	</span>
+</div>
+
+<style>
+	.select-container {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+		margin: auto;
+		height: 30vh;
+		width: 20vw;
+		background-color: rgb(243, 243, 243);
+		border: 1px solid darkgrey;
+		border-radius: 20px;
+		box-shadow: 5px 5px 3px #cdcdcd;
+	}
+
+	select {
+		width: 200px;
+		height: 35px;
+		font-size: 18px;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import StatePicker from '$lib/components/StatePicker.svelte';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+	export let { activeState } = data;
+
+	// time to start planning because i didn't plan out the last one and its very messy and unorganized
+
+	// routes
+	// noncompete.com main page - home route
+	// noncompete.com/login - login screen for admin
+	// noncompte.com/admin - dashboard panel
+
+	// components - we will use the $lib alias to import our utilities and components
+	// I will keep this the same - state picker, state info, and state editor
+	// state picker - whole purpose is dropdown and export of active state
+	// state info - for main screen, to display all state info
+	// state editor - for dashboard editing, fields to edit
+	// I want to use the Mozilla Standard for formData and Request/Response standards
+	// $lib/sever - do we need server only code?
+
+	// storing state
+	// need to maintain an active state for displaying of info
+	// change in state picker -> main state change in app -> change flows down to state editor
+
+	// static
+	// we shouldnt need any static files as we will call everything from SupaBase
+</script>
+
+<h1>Root Route - This will be main page</h1>
+<a href="/login">Click Here for Login</a>
+
+<p>Here is the activeState: {JSON.stringify(activeState)}</p>
+
+<div>
+	<StatePicker bind:activeState />
+</div>
