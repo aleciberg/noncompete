9442d9be70e5bbdd6e56a536e6cb80474632f86b
Alec Iberg <ibergalec@gmail.com>
2023-09-08 10:38:48 -0700

    working on redired

--- src/routes/auth/callback/+server.ts
+++ src/routes/auth/callback/+server.ts
@@ -2,6 +2,7 @@
 import { redirect } from '@sveltejs/kit';
 
 export const GET = async ({ url, locals: { supabase } }) => {
+	console.log(url);
 	const code = url.searchParams.get('code');
 
 	if (code) {
@@ -10,5 +11,5 @@ export const GET = async ({ url, locals: { supabase } }) => {
 
 	console.log('in redirect');
 
-	throw redirect(303, '/admin'); // change redirect here
+	throw redirect(303, `${url.origin}/admin`); // change redirect here
 };
